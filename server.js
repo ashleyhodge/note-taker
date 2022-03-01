295719d9753688caaa50a58cb74f499ba4f21cbd
Ashley Hodge <ashhodge@comcast.net>
2022-03-01 18:53:52 -0500

    update PORT

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { notes } = require('./db/notes');
-
+const PORT = process.env.PORT || 3001
 const app = express();
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
-app.listen(3001, () => {
-    console.log(`Api server now on port 3001!`)
+app.listen(PORT, () => {
+    console.log(`Api server now on port ${PORT}!`)
 });
